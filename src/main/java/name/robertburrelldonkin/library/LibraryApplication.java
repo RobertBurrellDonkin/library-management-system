f865daadfdb02aea21a824ed121811d0da0644dd
Robert Burrell Donkin <107208+RobertBurrellDonkin@users.noreply.github.com>
2024-10-21 20:43:47 +0100

    Generate skeleton application using Spring Initializr

---
+++ src/main/java/name/robertburrelldonkin/library/LibraryApplication.java
@@ -0,0 +1,13 @@
+package name.robertburrelldonkin.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplication.class, args);
+	}
+
+}
