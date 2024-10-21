6c8ba03361770ff9fed6d0680289961445f8763e
Robert Burrell Donkin <107208+RobertBurrellDonkin@users.noreply.github.com>
2024-10-21 21:23:02 +0100

    Initial design for Book domain object.

--- README.md
+++ README.md
@@ -1,12 +1,31 @@
 # Library Management System with RESTful API
 
-## Assumptions
+## Assumptions And Design Decisions
 * Java 8 is now end of life, has known security vulnerability 
 and is not supported by SpringBoot 3. The language used is consistently
 **Java 8+** (rather than **Java 8**). Let's assume Java 17 is an acceptable compromise.
 * Let's assume that library is intended to be reused at the microservice level by
 calling the RESTful API, rather than as a Java library. So no need for a multi-module
 set up.
+* We could model Book as a value object using a Record. This lends itself to an
+event driven architecture which would have advantages in a distributed system
+context. 
+  * Let's keep things simple for now by assuming that we're aiming at first
+  to create a singleton microservice backed by an ephemeral library class. 
+  This is a classic choice for rapid prototyping a proof of concept. We should be mindful around SOLID principles since we're likely to
+  want to be able to switch out this implementation for a data store (either RDBMS or NoSQL) 
+  or else some sort of distributed cache for production.
+* We'll model as a classic POJO. Let's assume that only available copies is
+mutable. At some later stage, should the API evolve to include additional
+mutation methods, we can add that to the domain object. 
+  * The Library API includes a method that passes in a book object. Interesting 
+  design question about whether to allow subclasses. I tend to prefer making objects 
+  final in case of doubt around whether the API is intended to allow subclassing.
+  Let's go with that for now.
+  * There are a lot of parameters with the same type. Let's add a builder to increase
+  readability.
+  
+
 
 
 
---
+++ src/main/java/name/robertburrelldonkin/library/domain/Book.java
@@ -0,0 +1,94 @@
+package name.robertburrelldonkin.library.domain;
+
+public final class Book {
+
+    public static Builder aBook() {
+        return new Builder();
+    }
+
+    private final String isbn;
+    private final String title;
+    private final String author;
+    private final int publicationYear;
+    private final int availableCopies;
+
+    private Book(String isbn,
+                 String title,
+                 String author,
+                 int publicationYear,
+                 int availableCopies) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.availableCopies = availableCopies;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public int getAvailableCopies() {
+        return availableCopies;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", publicationYear=" + publicationYear +
+                ", availableCopies=" + availableCopies +
+                '}';
+    }
+
+    public static final class Builder {
+        private String isbn;
+        private String title;
+        private String author;
+        private int publicationYear;
+        private int availableCopies;
+
+        public Builder withIsbn(String isbn) {
+            this.isbn = isbn;
+            return this;
+        }
+
+        public Builder withTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder withAuthor(String author) {
+            this.author = author;
+            return this;
+        }
+
+        public Builder withPublicationYear(int publicationYear) {
+            this.publicationYear = publicationYear;
+            return this;
+        }
+
+        public Builder withAvailableCopies(int availableCopies) {
+            this.availableCopies = availableCopies;
+            return this;
+        }
+
+        public Book build() {
+            return new Book(isbn, title, author, publicationYear, availableCopies);
+        }
+    }
+}
