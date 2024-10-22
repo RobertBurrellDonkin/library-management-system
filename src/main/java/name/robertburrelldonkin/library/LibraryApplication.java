be57f325e172e51d59b151f5b56fe66effe3c9ae
Robert Burrell Donkin <107208+RobertBurrellDonkin@users.noreply.github.com>
2024-10-22 11:08:56 +0100

    Add basic service layer to application

--- src/main/java/name/robertburrelldonkin/library/LibraryApplication.java
+++ src/main/java/name/robertburrelldonkin/library/LibraryApplication.java
@@ -1,7 +1,12 @@
 package name.robertburrelldonkin.library;
 
+import name.robertburrelldonkin.library.caches.LeastRecentlyUsedBookCache;
+import name.robertburrelldonkin.library.services.CachingLibraryManagementService;
+import name.robertburrelldonkin.library.services.Library;
+import name.robertburrelldonkin.library.services.LibraryManagementService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LibraryApplication {
@@ -10,4 +15,19 @@ public class LibraryApplication {
 		SpringApplication.run(LibraryApplication.class, args);
 	}
 
+	@Bean
+	public Library library() {
+		return new Library();
+	}
+
+	@Bean
+	public LeastRecentlyUsedBookCache cache() {
+		//TODO: externalise configuration
+		return new LeastRecentlyUsedBookCache(128, 24 , 0.75f);
+	}
+
+	@Bean
+	public LibraryManagementService libraryManagementService(Library library, LeastRecentlyUsedBookCache cache) {
+		return new CachingLibraryManagementService(library, cache);
+	}
 }
